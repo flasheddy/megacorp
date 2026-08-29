27d6c433927e9a38bbb658ab16bb0be441fcc311
flasheddy <326801595@qq.com>
2026-08-29 16:03:09 +0800

    P: revert adding scanner
    This reverts commit 889a7f2f9cb584a62e7a8683d2a8212af8c24bb6.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,15 +1,1 @@
-# Scan credit card numbers
-printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="
-
-# Scan social security numbers
-printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
-
-# Scan phone numbers
-printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "========= PHONE NUMBER SCAN COMPLETE ========="
+# TODO: write the script
